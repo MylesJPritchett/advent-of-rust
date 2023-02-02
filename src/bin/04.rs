698b7162dd78c5188f1eceb05caa48e3a4d417a2
Myles <mylesjpritchett@gmail.com>
2023-02-02 13:20:57 +1100

    finish day 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,9 +1,33 @@
-pub fn part_one(_input: &str) -> Option<u32> {
-    None
+use advent_of_code::helpers::parse_regex::parse_lines;
+use regex::Regex;
+
+pub fn part_one(input: &str) -> Option<usize> {
+    // read line
+    // if (first[0] <= second[0] and first[1] >= second[1]) or (second[0] <= first[0] and second[1] >= first[1])
+    // add 1 to count
+    let re = Regex::new(r"(\d+)\-(\d+),(\d+)\-(\d+)").unwrap();
+
+    let input: Vec<(usize, usize, usize, usize)> = parse_lines(re, input.trim()).collect();
+
+    Some(
+        input
+            .iter()
+            .filter(|(a1, a2, b1, b2)| (a1 <= b1 && a2 >= b2) || (b1 <= a1 && b2 >= a2))
+            .count(),
+    )
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let re = Regex::new(r"(\d+)\-(\d+),(\d+)\-(\d+)").unwrap();
+
+    let input: Vec<(usize, usize, usize, usize)> = parse_lines(re, input.trim()).collect();
+
+    Some(
+        input
+            .iter()
+            .filter(|(a1, a2, b1, b2)| a2 >= b1 && b2 >= a1)
+            .count(),
+    )
 }
 
 fn main() {
@@ -19,12 +43,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 4);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(2));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 4);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(4));
     }
 }
